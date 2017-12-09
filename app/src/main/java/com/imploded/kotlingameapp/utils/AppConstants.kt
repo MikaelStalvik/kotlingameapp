6ec4b8470cd254f7a97d737c32e2708c5ea3a91b
Mikael <mikael.stalvik@gmail.com>
2017-12-09 21:58:19 +0100

    Revised URLs

--- app/src/main/java/com/imploded/kotlingameapp/utils/AppConstants.kt
+++ app/src/main/java/com/imploded/kotlingameapp/utils/AppConstants.kt
@@ -2,6 +2,6 @@ package com.imploded.kotlingameapp.utils
 
 object AppConstants {
     val LoginUrl = "http://kotlinserver.azurewebsites.net/login"
-    fun DetailUrl(id: String) = "http://kotlinserver.azurewebsites.net/$id"
-    val GetGamesUrl = "http://kotlinserver.azurewebsites.net/list"
+    fun DetailUrl(id: String) = "http://kotlinserver.azurewebsites.net/games/$id"
+    val GetGamesUrl = "http://kotlinserver.azurewebsites.net/games"
 }
